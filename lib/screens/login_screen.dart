2f2fb7bd1b7f708d7678a4ac8e05c94e3ce5d7e8
Victor Marquez <marquez.pevic21@cadiz.salesianos.edu>
2022-11-17 11:35:00 +0100

    commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:flutter/scheduler.dart';
+import 'dart:async';
+import 'package:trabajo_flutter/widgets/stagger_animation.dart';
 
-import '../models/models.dart';
 import '../providers/login_form_provider.dart';
 import '../services/login_services.dart';
 import '../services/services.dart';
-import '../services/user_service.dart';
+
 import '../widgets/widgets.dart';
 import 'package:provider/provider.dart';
-
-import 'screens.dart';
-
-BuildContext contexto = contexto;
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    contexto = context;
     return Scaffold(
         backgroundColor: Colors.white,
         body: AuthBackground(
@@ -34,7 +30,8 @@ class LoginScreen extends StatelessWidget {
                 const SizedBox(height: 10),
                 ChangeNotifierProvider(
                   create: (_) => LoginFormProvider(),
-                  child: _LoginForm(),
+                  //  child: _LoginForm(),
+                  child: StaggerDemo1(),
                 )
               ],
             )),
@@ -47,7 +44,7 @@ class LoginScreen extends StatelessWidget {
                 style: ButtonStyle(
                     overlayColor: MaterialStateProperty.all(
                         Colors.indigo.withOpacity(0.1)),
-                    shape: MaterialStateProperty.all(StadiumBorder())),
+                    shape: MaterialStateProperty.all(const StadiumBorder())),
                 child: const Text(
                   'Create new account',
                   style: TextStyle(
@@ -125,6 +122,8 @@ class _LoginForm extends StatelessWidget {
                       // ignore: use_build_context_synchronously
                       Navigator.pushNamed(context, 'user');
                     } else {
+                      StaggerDemo1();
+                      /*
                       showModalBottomSheet(
                           context: context,
                           builder: (BuildContext context) {
@@ -167,12 +166,271 @@ class _LoginForm extends StatelessWidget {
                                 ),
                               ),
                             );
-                          });
+                          });*/
                     }
                   }
                 },
                 child: const Center(child: Text('Login')),
               ),
+            ),
+          ],
+        ));
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  var message;
+
+  StaggerAnimation({
+    Key? key,
+    required this.controller,
+    this.message,
+  })  :
+
+        // Each animation defined here transforms its value during the subset
+        // of the controller's duration defined by the animation's interval.
+        // For example the opacity animation transforms its value during
+        // the first 10% of the controller's duration.
+
+        opacity = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.0,
+              0.100,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        width = Tween<double>(
+          begin: 50.0,
+          end: 250.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.125,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 50.0, end: 50).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16.0),
+          end: const EdgeInsets.only(bottom: 16.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4.0),
+          end: BorderRadius.circular(75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.375,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        color = ColorTween(
+          begin: Colors.red,
+          end: Colors.red,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: const Interval(
+              0.200,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        super(key: key);
+
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation borderRadius;
+  final Animation color;
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget buildAnimation(BuildContext context, Widget? child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+            color: color.value,
+            border: Border.all(
+              color: Colors.black,
+              width: 2.0,
+            ),
+            borderRadius: borderRadius.value,
+          ),
+          child: Center(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(Icons.error_outline),
+              Text('ERROR TO LOGIN', style: TextStyle(color: Colors.white)),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: buildAnimation,
+      animation: controller,
+    );
+  }
+}
+
+class StaggerDemo1 extends StatefulWidget {
+  @override
+  _StaggerDemo1State createState() => _StaggerDemo1State();
+}
+
+class _StaggerDemo1State extends State<StaggerDemo1>
+    with TickerProviderStateMixin {
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller!.forward().orCancel;
+      await _controller!.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 1.0; // 1.0 is normal animation speed.
+
+    final loginService = Provider.of<LoginServices>(context);
+    final loginForm = Provider.of<LoginFormProvider>(context);
+    String? message;
+    return Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [
+            TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    hintText: 'User email',
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.alternate_email_rounded),
+                    border: OutlineInputBorder(),
+                    iconColor: Color.fromRGBO(0, 153, 153, 1)),
+                onChanged: (value) => loginForm.email = value,
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '') ? null : 'Insert email';
+                }),
+            const SizedBox(height: 20),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              decoration: const InputDecoration(
+                  hintText: 'User password',
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                  iconColor: Color.fromRGBO(0, 153, 153, 1)),
+              onChanged: (value) => loginForm.password = value,
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                  fixedSize: MaterialStateProperty.all(
+                      const Size(double.infinity, 30)),
+                ),
+                onPressed: () async {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  if (loginForm.isValidForm()) {
+                    final String? errorMessage = await loginService.postLogin(
+                        loginForm.email, loginForm.password);
+                    //message = errorMessage;
+                    if (errorMessage == 'a') {
+                      // message = errorMessage;
+                      // ignore: use_build_context_synchronously
+
+                      Navigator.pushNamed(context, 'index');
+                    } else if (errorMessage == 'u') {
+                      //  message = errorMessage;
+                      // ignore: use_build_context_synchronously
+
+                      // ignore: use_build_context_synchronously
+                      Navigator.pushNamed(context, 'user');
+                    } else {
+                      message = errorMessage;
+                      _playAnimation();
+                    }
+                  }
+                },
+                child: const Center(child: Text('Login')),
+              ),
+            ),
+            StaggerAnimation(
+              controller: _controller!.view,
+              message: message,
             ),
           ],
         ));
